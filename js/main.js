c25ddd48f684aedf7dc771c08c32290530a29982
JeungWookEun <e_nfoo@naver.com>
2021-06-06 23:08:34 +0900

    .

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//main 표시
+function showsubway() {
+  document.getElementById("en1"). src = "img/영어차이/subway.png";
+  document.getElementById("en2"). innerHTML = "대부분 ‘지하철’을 영어로 말할 때 가장 먼저 떠오르는 단어가 ‘subway’일 것 같지만 영국에서 “where is the subway station?”이라고 질문하면 우리가 흔히 알고 있는 샌드위치 매장 ‘subway’에 데려다주는 웃픈 상황이 생길 수도 있다고 합니다. 영국에서 ‘지하철’은 'tube' 혹은 'underground'라는 단어를 사용하기 때문인데요. 지하철의 모양새가 튜브처럼 생겨서 붙여졌다고 하네요! ";
+}
+function showchips() {
+  document.getElementById("en1"). src = "img/영어차이/chips.png" 
+  document.getElementById("en2"). innerHTML = "포테이토칩과 같이 얇은 과자를 부를 때 미국에서 ‘chips’라는 단어를 사용합니다.하지만 영국에서는 다른 의미로 쓰인다고 하는데요.영국의 대표 음식인 'fish and chips에서 그 힌트를 얻을 수 있듯이 영국에서 ‘chips’는 감자튀김을 말합니다.얇은 포테이토칩은 영국에서 ‘crisps’라고 부른다고 해요. ";
+}
+function showfirstfloor() {
+  document.getElementById("en1"). src = "img/영어차이/firstfloor.png";
+  document.getElementById("en2"). innerHTML = "'first floor'라고 하면 직관적으로 '1층'이라고 생각되지 않으신가요? 우리의 예상대로 미국에서는 '1층'이 맞지만 영국을 포함한 유럽권에서는 '2층'을 뜻하는데요. 유럽에서는 '1층'을 'ground floor’라고 하기 때문에 2층부터 'first floor', 'second floor'(3층), 'third floor'(4층)라고 부르면 됩니다. 층수가 헷갈리기 쉬우니 영국을 포함한 유럽권에서 엘리베이터를 타실 때 특히 주의해주세요~! ";
+}
+function showpants() {
+  document.getElementById("en1"). src = "img/영어차이/pants.png";
+  document.getElementById("en2"). innerHTML = "바지를 사기 위해서 영국 의류 매장에 들어가서 'I want to buy pants'라고 말하면 '바지'대신 '속옷'을 받게 되는데요.영국에서 ‘pants’는 '속옷'을 부를 때만 사용하기 때문이죠. 영국에서 '바지'는 'trousers'라는 단어를 사용합니다. 여기서 한가지 더! 스코틀랜드 단어로 '긴 바지'라는 뜻을 가진 'trews'에서 파생되어 영국에서 'trousers'라고 부르게 되었다고 하네요.";
+}
+function showvest() {
+  document.getElementById("en1"). src = "img/영어차이/vest.png";
+  document.getElementById("en2"). innerHTML = "미국에서는 'vest'가 '양복 조끼'를 뜻하는 반면, 영국에서는 '메리야스'를 의미합니다. 같은 단어라도 나라에 따라서 외출용 의류와 내의 옷이 결정되는데요. 그렇다면 '양복 조끼'를 부를 때 영국에서는 어떤 단어를 사용할까요? 18세기 영국에서 긴 코트가 대부분이었다가 코트를 허리 높이로 짧게 자르는 것이 패션이 되면서 'waist coat' 단어를 사용하게 되었다고 하네요.";
+}
+function hide() {
+  document.getElementById("en1").src = "";
+  document.getElementById("en2").innerHTML ="";
+}
---
+++ js/proflie.js
@@ -0,0 +1,10 @@
+//프로필
+
+function showmango() {
+document.getElementById("mango1"). src= "img/프로필/망고.jpg";
+document.getElementById("mango0"). innerHTML = "저희 집의 귀여운 막내 망고입니다."
+}
+function hide() {
+  document.getElementById("mango1"). src = "";
+  document.getElementById("mango0"). innerHTML ="";
+}
